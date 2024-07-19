0cb5e859cec735f0d43c0db026fa5e3408401429
pierregraves <86963779+pierregraves@users.noreply.github.com>
2024-07-19 10:04:48 +0100

    Add files via upload

---
+++ data/jojos_objects_3.js
@@ -0,0 +1,1 @@
+var json_jojos_objects_3 = {"type":"FeatureCollection","name":"jojos_objects_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[-0.977532874768496,51.596471747900253]}}]}
